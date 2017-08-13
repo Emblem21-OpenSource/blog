1917963afd1f75b26e120bd7dea0d5cc88ffc635
codeotter <patrick.ryan@emblem21.com>
2017-08-13 04:20:21 -0700

    Making sales a thing

--- themes/weightless/source/js/fn.js
+++ themes/weightless/source/js/fn.js
@@ -1,51 +1,49 @@
-var index,
-	store;
+var index;
+var store;
 
 function loadJQuery(href, e) {
-	e.preventDefault();
-	if (!window.jQuery) {
-		var script = document.createElement('script');
-		script.type = 'text/javascript';
-		script.src = jQueryJS;
-		document.getElementsByTagName("head")[0].appendChild(script);
-		var checkReady = function(callback) {
-	        if (window.jQuery) {
-	            callback(jQuery);
-	        }
-	        else {
-	            window.setTimeout(function() { checkReady(callback); }, 100);
-	        }
-	    };
-		checkReady(function() {
-			loadMore(href);
-		});
-	}else{
-		loadModal(href);
-	}
+  e.preventDefault();
+  if (!window.jQuery) {
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = jQueryJS;
+    document.getElementsByTagName("head")[0].appendChild(script);
+    var checkReady = function (callback) {
+      if (window.jQuery) {
+          callback(jQuery);
+      } else {
+          window.setTimeout(function () { checkReady(callback); }, 100);
+      }
+    };
+    checkReady(function () {
+      loadMore(href);
+    });
+  } else {
+    loadModal(href);
+  }
 }
 
-function loadMore(href) {
-	$.when(
-		$.getScript(lunrJS),
-		$.getScript(modalJS),
-		$.Deferred(function(deferred){
-			$( deferred.resolve );
-		})
-	).done(function(){
-		loadLunr(href);
-	});
+function loadMore (href) {
+  $.when(
+    $.getScript(lunrJS),
+    $.getScript(modalJS),
+    $.Deferred(function (deferred){
+      $( deferred.resolve );
+    })
+  ).done(function (){
+    loadLunr(href);
+  });
 }
 
-function loadLunr(href) {
+function loadLunr (href) {
+  $.getJSON(lunrAsset, function (response) {
+    store = response.store,
+    index = lunr.Index.load(response.index);
 
-	$.getJSON(lunrAsset, function(response) {
-		store = response.store,
-		index = lunr.Index.load(response.index);
-
-		loadModal(href);
-	});
+    loadModal(href);
+  });
 }
 
-function loadModal(href) {
-	$(href).modal();
+function loadModal (href) {
+  $(href).modal();
 }
